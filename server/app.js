3039dd9d245b9e31a25082297dce488796345f27
Victor Mosqueda <vdmc_123@hotmail.com>
2026-01-12 16:47:19 +0000

    day 1 of mini project done

--- server/app.js
+++ server/app.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const dataCohorts = require("./cohorts.json");
+const dataStudents = require("./students.json");
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
-
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-// ...
+const cors = require("cors");
+app.use(cors());
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
@@ -29,6 +30,13 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res) => {
+  res.json(dataCohorts);
+});
+
+app.get("/api/students", (req, res) => {
+  res.json(dataStudents);
+});
 
 // START SERVER
 app.listen(PORT, () => {
